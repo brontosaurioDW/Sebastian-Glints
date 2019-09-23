b93cb0068a0937f98f40cd01c33d1cee53c619f9
florsepulveda <fsepulveda@perceptyx.com>
2019-09-23 16:03:29 -0300

    primer commit

---
+++ dev/js/main.js
@@ -0,0 +1,56 @@
+// INIT AOS
+AOS.init({
+  duration: 1200,
+})
+
+// Add a general loading
+$(window).on("load",function(){
+     $(".loader").fadeOut("slow");
+});
+
+// Menu
+(function() {
+	var container = document.querySelector( '.sg-wrapper' ),
+		triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( '.overlay' ),
+		closeBttn = overlay.querySelector( '.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.remove( container, 'overlay-open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+			classie.add( container, 'overlay-open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
+
+// Scroll
