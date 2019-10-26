f573c721a23d31a211c6379060bab5652b7e6e44
florsepulveda <fsepulveda@perceptyx.com>
2019-10-26 17:26:11 -0300

    fullpage.js de nuevo ¬¬

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,56 +1,73 @@
 // INIT AOS
 AOS.init({
-  duration: 1200,
+    duration: 1200,
 })
 
 // Add a general loading
-$(window).on("load",function(){
-     $(".loader").fadeOut("slow");
+$(window).on("load", function() {
+    $(".loader").fadeOut("slow");
 });
 
 // Menu
 (function() {
-	var container = document.querySelector( '.sg-wrapper' ),
-		triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( '.overlay' ),
-		closeBttn = overlay.querySelector( '.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
+    var container = document.querySelector('.sg-wrapper'),
+        triggerBttn = document.getElementById('trigger-overlay'),
+        overlay = document.querySelector('.overlay'),
+        closeBttn = overlay.querySelector('.overlay-close');
+    transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+        support = {
+            transitions: Modernizr.csstransitions
+        };
 
-	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			classie.remove( overlay, 'open' );
-			classie.remove( container, 'overlay-open' );
-			classie.add( overlay, 'close' );
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName !== 'visibility' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				classie.remove( overlay, 'close' );
-			};
-			if( support.transitions ) {
-				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
-		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			classie.add( overlay, 'open' );
-			classie.add( container, 'overlay-open' );
-		}
-	}
+    function toggleOverlay() {
+        if (classie.has(overlay, 'open')) {
+            classie.remove(overlay, 'open');
+            classie.remove(container, 'overlay-open');
+            classie.add(overlay, 'close');
+            var onEndTransitionFn = function(ev) {
+                if (support.transitions) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener(transEndEventName, onEndTransitionFn);
+                }
+                classie.remove(overlay, 'close');
+            };
+            if (support.transitions) {
+                overlay.addEventListener(transEndEventName, onEndTransitionFn);
+            } else {
+                onEndTransitionFn();
+            }
+        } else if (!classie.has(overlay, 'close')) {
+            classie.add(overlay, 'open');
+            classie.add(container, 'overlay-open');
+        }
+    }
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+    triggerBttn.addEventListener('click', toggleOverlay);
+    closeBttn.addEventListener('click', toggleOverlay);
 })();
 
 // Scroll
+$(document).ready(function() {
+	new fullpage('#fullpage', {
+		/*menu: '#menu',
+		anchors:['cover', 'song-1', 'song-2', 'song-3', 'song-4', 'song-5', 'song-6', 'song-7', 'song-8', 'song-9', 'song-10', 'song-11', 'song-12', 'song-13',],*/
+		navigation: true,
+		navigationPosition: 'left',
+
+		scrollOverflow: true,
+		scrollOverflowReset: true,
+		scrollingSpeed: 1000,
+		fadingEffect: true,
+		parallax: true,
+		css3: false,
+		scrollbars: true,
+		licenseKey: '58C9F4E3-BB01438E-A94640F4-C5A13204'
+	});
+});
